a6c66ba1e5c94b2404bf6583edd6952db752e71f
ricardoreiss <112704409+ricardoreiss@users.noreply.github.com>
2023-06-05 11:32:19 -0300

    Add files via upload

---
+++ tests_part2.py
@@ -0,0 +1,47 @@
+
+def organize_text(text):
+    organized_text = []
+    for c in text:
+        if c:
+            c = c.strip()
+            c = c.split(' ')
+            organized_text.append(c)
+    return organized_text
+
+def organize_parameters(organized_text):
+    parameters = []
+    for l in organized_text:
+        for id2, w in enumerate(l):
+            comp = []
+            for pos in range(l.index(w)+1, len(l)+1):
+                print(l[pos])
+                seg = [w, l[pos]]
+                comp.append(seg)
+            parameters.append(comp)
+
+    return parameters
+
+
+text = """A amizade consegue ser tão complexa.
+Deixa uns desanimados, outros bem felizes.
+É a alimentação dos fracos
+É o reino dos fortes.
+
+Faz-nos cometer erros
+Os fracos deixam se ir abaixo
+Os fortes erguem sempre a cabeça
+Os assim assumem-nos.
+
+Sem pensar conquistamos
+o mundo geral
+e construímos o nosso pequeno lugar,
+deixando brilhar cada estrelinha.
+
+Estrelinhas
+Doces, sensíveis, frias, ternurentas.
+Mas sempre presentes em qualquer parte.
+Os donos da amizade."""
+text = text.replace('\n', ' ').replace(',', '').lower().split('.')
+organized_text = organize_text(text)
+#print(organized_text)
+print(organize_parameters(organized_text))
